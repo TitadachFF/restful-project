57caf75e97305a5648898cce4a4f066fa5b2d9d7
TASAHOST <120707750+TASAHOST@users.noreply.github.com>
2023-11-09 10:14:18 +0700

    config file for connect

---
+++ controller/restaurant.controller.js
@@ -0,0 +1,62 @@
+const Restaurant = require('../models/restaurant.model');
+
+const Restaurantscontroller =  {
+    getAllRestaurant: async (req, res) =>{
+        try {
+            const restaurant = await Restaurant.find();
+            res.json(restaurant); 
+        } catch (error) {
+            return(error);
+        }
+    },
+
+    getByIdRestaurant: async (req, res) =>{
+        const restaurantID = req.params.id;
+
+            try {
+                const restaurant = await Restaurant.findById(restaurantID);
+                res.json(restaurant);
+            } catch (error) {
+                return(error);
+            }
+    },
+
+    createRestaurant: async(req,res)=> {
+        const {name, type, price, image} = req.body;
+        const newRestaurant = new Restaurant({name, type, price, image});
+
+        try {
+            const restaurant = await newRestaurant.save();
+            res.json(restaurant);
+        } catch (error) {
+            return(error);
+        }
+    },
+
+    deleteRestaurant: async(req, res) =>{
+        const restaurantID = req.params.id;
+
+        try {
+            const restaurant = await Restaurant.findByIdAndDelete(restaurantID);
+            res.json({message:"Deleted"});
+        } catch (error) {
+            return(error);
+        }
+    },
+
+    updateRestaurant: async(req, res) =>{
+        const restaurantID = req.params.id;
+        const {name, type, price, image} = req.body;
+
+        try {
+            const uprestaurant = await Restaurant.findByIdAndUpdate(restaurantID, {name, type, price, image});
+            res.json(uprestaurant);
+        } catch (error) {
+            return(error);
+        }
+    }
+    
+
+};
+
+module.exports = Restaurantscontroller;
---
+++ routes/restaurant.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const Restaurantscontroller = require('../controller/restaurant.controller');
+
+router.get("/" , Restaurantscontroller.getAllRestaurant);
+router.get("/:id" , Restaurantscontroller.getByIdRestaurant);
+router.post("/" , Restaurantscontroller.createRestaurant);
+router.delete("/:id" , Restaurantscontroller.deleteRestaurant);
+router.put("/:id" , Restaurantscontroller.updateRestaurant);
+
+module.exports = router;
